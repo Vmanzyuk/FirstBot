6d1be1cf50a92bb4bdd00759a687db855291eb26
Vladimir <Vmanzyuk@outlook.com>
2018-09-16 16:39:53 +0300

    First release

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
 import logging
 import settings
+import datetime
+import ephem
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -18,6 +20,23 @@ def talk_to_me(bot, update):
     print(user_text)
     update.message.reply_text(user_text)
 
+def planets(bot,update):
+    if update.message.text=='/planet':
+        update.message.reply_text('Pls enter the planet name after "/planet" like Mars, Jupiter, Venus e.t.c')
+    else:
+        a,planet_name=update.message.text.split(' ')
+        date=datetime.datetime.now()
+        datestr=date.strftime('%Y.%m.%d')
+        
+        try:
+            planet_position=getattr(ephem,planet_name)(datestr)
+            print (planet_position)
+            ans=ephem.constellation(planet_position)[1]
+            update.message.reply_text(ans)
+        except(AttributeError):
+            update.message.reply_text('Pls enter the planet name after "/planet" like Mars, Jupiter, Venus e.t.c')    
+    
+
 def main():
     mybot= Updater(settings.API_key,request_kwargs=settings.PROXY)
     
@@ -26,6 +45,7 @@ def main():
     dp=mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('planet',planets))
 
     mybot.start_polling()
     mybot.idle()
